ec490045fb6dd272a416fbb9895846a45d835dc8
Robert Ballou <havoc@ballou-pc.lan>
2022-08-11 21:27:18 -0700

    Working on displaying math result in realtime. Math for multiply and add work in real time. Next will be for the other operators. Then I need to fix an issue with how its calculating after you hit equals and want to do more calculations.
    Added color to buttons since iphone was defaulting to blue.
    Disabled tap to zoom so it doesnt zoom in on phone when trying to
    use calculator.

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+let displayDiv = document.getElementById('display');
+let mathP = document.getElementById('math');
+let totalP = document.getElementById('total');
 let display;
+let total = false;
 let lockedNum;
+let totalNum;
 let number = '';
 let operator = '';
 
 function test(){
     console.log(`
     lockedNum is: ${lockedNum} 
-    number is: ${number} 
-    operator is: ${operator}`)
+    number is: ${number}
+    totalNum is: ${totalNum}
+    operator is: ${operator}
+    total is: ${total}`)
 };
 
 function add(x, y){
@@ -28,20 +35,38 @@ function divide(x, y){
 
 function operate(operator, x, y){
     if(operator === 'add'){
-        lockedNum = add(x, y);
-        number = '';
-        operator = '';
-        return lockedNum;
+        if(total){
+            lockedNum = add(x, y);
+            number = '';
+            operator = '';
+            total = false;
+            return lockedNum;
+        } else if(total === false) {
+            totalNum = add(x, y);
+            return totalNum;
+        }
+
+
     }else if(operator === 'subtract'){
         lockedNum = subtract(x, y);
-        number = '';
-        operator = '';
+        // number = '';
+        // operator = '';
         return lockedNum;
+
+
     }else if (operator === 'multiply'){
-        lockedNum = multiply(x, y);
-        number = '';
-        operator = '';
-        return lockedNum
+        if(total){
+            lockedNum = multiply(x,y);
+            number = '';
+            operator = '';
+            total = false;
+            return lockedNum;
+        } else if(!total){
+            totalNum = multiply(x,y);
+            return totalNum;
+        }
+
+
     }else if(operator === 'divide'){
         // if(y === 0){
         //     number = '';
@@ -50,32 +75,31 @@ function operate(operator, x, y){
         //     return 'To infinity and beyond!';
         // }else {
         lockedNum = divide(x, y);
-        number = '';
-        operator = '';
+        // number = '';
+        // operator = '';
         return lockedNum;
         // }
     }
 }
 
 document.getElementById('add').onclick = () => {
-    document.getElementById('display').textContent += '+';
-    if(operator){
-        operate(operator, lockedNum, (+number));
-    }
-    operator = 'add';
+    mathP.textContent += '+';
     if(!lockedNum){
         lockedNum = (+number);
         number = '';
-    }else if (lockedNum && number){
-        operate(operator, lockedNum, (+number));
+        operator = 'add';
+    }else if (lockedNum && number && operator !== 'add'){
+        lockedNum = operate(operator, lockedNum, (+number));
+        operator = 'add';
+        number ='';
+    } else if (lockedNum && number && operator === 'add'){
+        lockedNum = operate(operator, lockedNum, (+number));
+        number = "";
     }
 }
 
 document.getElementById('subtract').onclick = () => {
-    document.getElementById('display').textContent += '-';
-    if(operator){
-        operate(operator, lockedNum, (+number));
-    }
+    mathP.textContent += '-';
     operator = 'subtract';
     if(!lockedNum){
         lockedNum = (+number);
@@ -86,24 +110,23 @@ document.getElementById('subtract').onclick = () => {
 }
 
 document.getElementById('multiply').onclick = () => {
-    document.getElementById('display').textContent += '×';
-    if(operator){
-        operate(operator, lockedNum, (+number));
-    }
-    operator = 'multiply';
+    mathP.textContent += '×';
     if(!lockedNum){
         lockedNum = (+number);
         number = '';
-    }else if (lockedNum && number){
-        operate(operator, lockedNum, (+number));
+        operator = 'multiply';
+    }else if (lockedNum && number && operator !== 'multiply'){
+        lockedNum = operate(operator, lockedNum, (+number));
+        operator = 'multiply';
+        number = "";
+    } else if(lockedNum && number && operator === 'multiply'){
+        lockedNum = operate(operator, lockedNum, (+number));
+        number = '';
     }
 }
 
 document.getElementById('divide').onclick = () => {
-    document.getElementById('display').textContent += '÷';
-    if(operator){
-        operate(operator, lockedNum, (+number));
-    }
+    mathP.textContent += '÷';
     operator = 'divide';
     if(!lockedNum){
         lockedNum = (+number);
@@ -114,118 +137,131 @@ document.getElementById('divide').onclick = () => {
 }
 
 document.getElementById('equals').onclick = () => {
-    document.getElementById('display').textContent = operate(operator, lockedNum, (+number));
+    total = true;
+    mathP.textContent = operate(operator, lockedNum, (+number));
+    totalP.textContent = '';
 }
 
 document.getElementById('decimal').onclick = () => {
-    document.getElementById('display').textContent += '.';
+    mathP.textContent += '.';
     number += '.';
 }
 
 document.getElementById('b1').onclick = () => {
-    document.getElementById('display').textContent += 1;
+    mathP.textContent += 1;
     number += '1';
+    totalP.textContent = operate(operator, lockedNum, (+number));
 }
 
 document.getElementById('b2').onclick = () => {
-    document.getElementById('display').textContent += 2;
+    mathP.textContent += 2;
     number += '2';
+    totalP.textContent = operate(operator, lockedNum, (+number));
 }
 
 document.getElementById('b3').onclick = () => {
-    document.getElementById('display').textContent += 3;
+    mathP.textContent += 3;
     number += '3';
+    totalP.textContent = operate(operator, lockedNum, (+number));
 }
 
 document.getElementById('b4').onclick = () => {
-    document.getElementById('display').textContent += 4;
+    mathP.textContent += 4;
     number += '4';
+    totalP.textContent = operate(operator, lockedNum, (+number));
 }
 
 document.getElementById('b5').onclick = () => {
-    document.getElementById('display').textContent += 5;
+    mathP.textContent += 5;
     number += '5';
+    totalP.textContent = operate(operator, lockedNum, (+number));
 }
 
 document.getElementById('b6').onclick = () => {
-    document.getElementById('display').textContent += 6;
+    mathP.textContent += 6;
     number += '6';
+    totalP.textContent = operate(operator, lockedNum, (+number));
 }
 
 document.getElementById('b7').onclick = () => {
-    document.getElementById('display').textContent += 7;
+    mathP.textContent += 7;
     number += '7';
+    totalP.textContent = operate(operator, lockedNum, (+number));
 }
 
 document.getElementById('b8').onclick = () => {
-    document.getElementById('display').textContent += 8;
+    mathP.textContent += 8;
     number += '8';
+    totalP.textContent = operate(operator, lockedNum, (+number));
 }
 
 document.getElementById('b9').onclick = () => {
-    document.getElementById('display').textContent += 9;
+    mathP.textContent += 9;
     number += '9';
+    totalP.textContent = operate(operator, lockedNum, (+number));
 }
 
 document.getElementById('b0').onclick = () => {
-    document.getElementById('display').textContent += 0;
+    mathP.textContent += 0;
     number += '0';
+    totalP.textContent = operate(operator, lockedNum, (+number));
 }
 
 document.getElementById('clear').onclick = () => {
-    document.getElementById('display').textContent = '';
+    mathP.textContent = '';
+    totalP.textContent = '';
     lockedNum = '';
     number = '';
     operator = '';
 }
 
-window.addEventListener('keydown', (event) => {
-    let name = event.key;
-    let code = event.code;
-    console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
-    if(code === 'Numpad1'){
-        document.getElementById('display').textContent += 1;
-        number += '1';
-    } else if(code === 'Numpad2'){
-        document.getElementById('display').textContent += 2;
-        number += '2';
-    } else if(code === 'Numpad3'){
-        document.getElementById('display').textContent += 3;
-        number += '3';
-    } else if(code === 'Numpad4'){
-        document.getElementById('display').textContent += 4;
-        number += '4';
-    } else if(code === 'Numpad5'){
-        document.getElementById('display').textContent += 5;
-        number += '5';
-    } else if(code === 'Numpad6'){
-        document.getElementById('display').textContent += 6;
-        number += '6';
-    } else if(code === 'Numpad7'){
-        document.getElementById('display').textContent += 7;
-        number += '7';
-    } else if(code === 'Numpad8'){
-        document.getElementById('display').textContent += 8;
-        number += '8';
-    } else if(code === 'Numpad9'){
-        document.getElementById('display').textContent += 9;
-        number += '9';
-    } else if(code === 'Numpad0'){
-        document.getElementById('display').textContent += 0;
-        number += '0';
-    } else if(code === 'NumpadEnter'){
-        document.getElementById('display').textContent = operate(operator, lockedNum, (+number));
-    } else if(code === 'NumpadAdd'){
-        document.getElementById('display').textContent += '+';
-        if(operator){
-            operate(operator, lockedNum, (+number));
-        }
-        operator = 'add';
-        if(!lockedNum){
-            lockedNum = (+number);
-            number = '';
-        }else if (lockedNum && number){
-            operate(operator, lockedNum, (+number));
-        }
-    } 
-  }, false);
+// window.addEventListener('keydown', (event) => {
+//     let name = event.key;
+//     let code = event.code;
+//     console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
+//     if(code === 'Numpad1'){
+//         document.getElementById('display').textContent += 1;
+//         number += '1';
+//     } else if(code === 'Numpad2'){
+//         document.getElementById('display').textContent += 2;
+//         number += '2';
+//     } else if(code === 'Numpad3'){
+//         document.getElementById('display').textContent += 3;
+//         number += '3';
+//     } else if(code === 'Numpad4'){
+//         document.getElementById('display').textContent += 4;
+//         number += '4';
+//     } else if(code === 'Numpad5'){
+//         document.getElementById('display').textContent += 5;
+//         number += '5';
+//     } else if(code === 'Numpad6'){
+//         document.getElementById('display').textContent += 6;
+//         number += '6';
+//     } else if(code === 'Numpad7'){
+//         document.getElementById('display').textContent += 7;
+//         number += '7';
+//     } else if(code === 'Numpad8'){
+//         document.getElementById('display').textContent += 8;
+//         number += '8';
+//     } else if(code === 'Numpad9'){
+//         document.getElementById('display').textContent += 9;
+//         number += '9';
+//     } else if(code === 'Numpad0'){
+//         document.getElementById('display').textContent += 0;
+//         number += '0';
+//     } else if(code === 'NumpadEnter'){
+//         document.getElementById('display').textContent = operate(operator, lockedNum, (+number));
+//     } else if(code === 'NumpadAdd'){
+//         document.getElementById('display').textContent += '+';
+//         if(operator){
+//             operate(operator, lockedNum, (+number));
+//         }
+//         operator = 'add';
+//         if(!lockedNum){
+//             lockedNum = (+number);
+//             number = '';
+//         }else if (lockedNum && number){
+//             operate(operator, lockedNum, (+number));
+//         }
+//     } 
+//   }, false);
