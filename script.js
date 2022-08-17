b2203d80c5ae3354bdadf590150930d353860ae0
Robert Ballou <havoc@ballou-pc.lan>
2022-08-16 21:02:54 -0700

    undo button functionality and runops bugs fixed.

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let operator = '';
 // Debug help function
 function test(){
     console.log(`
+    mathP: ${mathP.textContent}
     lockedNum is: ${lockedNum} 
     number is: ${number}
     totalNum is: ${totalNum}
@@ -48,43 +49,67 @@ function divide(x, y){
 // function for event listeners to use so I dont have to rewrite for all operation events.
 function runOps(symbol, opName){
     mathP.textContent += symbol;
+
+        //runOps test #1
+        //if their is no lockedNum then number will be transfered to lockedNum. 
+        //number is cleared and operator is changed to whichever operator button was pressed.
         if(!lockedNum){
-            console.log(`1st runOps for symbol: ${symbol} & opName ${opName}`)
-            test()
+            console.log(`runOps test #1`);
+            test();
             lockedNum = (+number);
             number = '';
             operator = opName;
-            test()
+            test();
         }else if (lockedNum && !number && !operator){
-            console.log(`2nd runOps for symbol: ${symbol} & opName ${opName}`)
-            test()
+            console.log(`runOps test #2`);
+            test();
             operator = opName;
-            test()
+            test();
         }else if (lockedNum && number && operator !== opName && total){
-            console.log(`3rd runOps for symbol: ${symbol} & opName ${opName}`)
-            test()
+            console.log(`runOps test #3`);
+            test();
             total = false;
             operator = opName;
             number = "";
-            test()
+            test();
+
+        //runOps test #4
+        //if theirs a lockedNum, number, and the operator pressed doesnt match the current operator.
+        //then operate is ran with the current operation. That number is put into lockedNum.
+        //number is cleared and then the operator is changed to the new operator getting ready for a new calculation.
+        //this is usually activated with a chain of operations.
         }else if (lockedNum && number && operator !== opName){
-            console.log(`4th runOps for symbol: ${symbol} & opName ${opName}`)
-            test()
+            console.log(`runOps test #4`);
+            test();
             lockedNum = operate(operator, lockedNum, (+number));
             operator = opName;
             number ='';
-            test()
-        } else if (lockedNum && number && operator === opName){
-            console.log(`5th runOps for symbol: ${symbol} & opName ${opName}`)
-            test()
+            test();
+
+        //runOps test #$
+        //added this if statement because if i hit 5x5 for example and kept pressing enter to keep multiplying the total by 5.
+        //then I tried to then i tried to multiply that by something else. it would multiply lockedNum by number. 
+        //even though it should be clearing the number to get ready for a new calculation.
+        //also clearing the total since we aren hitting the equals button anymore.
+        } else if (total && lockedNum && number && operator === opName){
+            console.log(`runOps test #5`);
+            test();
+            number = '';
+            total = false;
+            test();
+
+            
+        } else if(lockedNum && number && operator === opName){
+            console.log(`runOps test #6`);
+            test();
             lockedNum = operate(operator, lockedNum, (+number));
             number = "";
-            test()
+            test();
         } else if (lockedNum && operator && total){
-            console.log(`6th runOps for symbol: ${symbol} & opName ${opName}`)
-            test()
+            console.log(`runOps test #7`);
+            test();
             total = false;
-            test()
+            test();
         }
 }
 
@@ -95,26 +120,26 @@ function operate(operator, x, y){
     for(let i = 0; i < opArray.length; i++){
         if(operator === opNames[i]){
             if(total && number){
-                console.log(`1st operate function for ${operator}, ${x}, ${y}`)
-                test()
+                console.log(`operate function test #1`);
+                test();
                 lockedNum = opArray[i];
                 totalNum = '';
-                test()
+                test();
                 return lockedNum;
             } else if(total){
-                console.log(`2nd operate function for ${operator}, ${x}, ${y}`)
-                test()
+                console.log(`operate function test #2`);
+                test();
                 operator = opNames[i];
                 totalNum = '';
                 number = '';
                 total = false; 
-                test()
+                test();
                 return lockedNum;
             } else if (total === false){
-                console.log(`3rd operate function for ${operator}, ${x}, ${y}`)
-                test()
+                console.log(`operate function test #3`);
+                test();
                 totalNum = opArray[i];
-                test()
+                test();
                 return totalNum;
             }
         }
@@ -125,7 +150,11 @@ function operate(operator, x, y){
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach((button) => {
     button.onclick = () => {
-        if(mathP.textContent.slice(-1) === '+' || mathP.textContent.slice(-1) === '-' || mathP.textContent.slice(-1) === '÷' || mathP.textContent.slice(-1) === '×'){
+        if (total && lockedNum && operator){
+            runOps(button.textContent, button.id);
+        } else if (!lockedNum && !number){
+            return;
+        } else if(mathP.textContent.slice(-1) === '+' || mathP.textContent.slice(-1) === '-' || mathP.textContent.slice(-1) === '÷' || mathP.textContent.slice(-1) === '×'){
             return;
         } else {
             runOps(button.textContent, button.id);
@@ -137,9 +166,18 @@ operatorButtons.forEach((button) => {
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach((button) => {
     button.onclick = () => {
-        mathP.textContent += button.textContent;
-        number += button.textContent;
-        totalP.textContent = operate(operator, lockedNum, (+number));
+        if(total){
+            number = lockedNum;
+            lockedNum = '';
+            mathP.textContent += button.textContent;
+            number += button.textContent;
+            totalP.textContent = '';
+            total = false;
+        } else {
+            mathP.textContent += button.textContent;
+            number += button.textContent;
+            totalP.textContent = operate(operator, lockedNum, (+number));
+        }
     }
 });
 
@@ -164,6 +202,13 @@ document.getElementById('clear').onclick = () => {
 }
 
 document.getElementById('delete').onclick = () => {
+    if (total){
+        number = lockedNum.toString();
+        lockedNum = '';
+        totalP.textContent = '';
+        operator = '';
+        total = false;
+    }
     if (number.length > 0){
         number = number.slice(0, -1);
         mathP.textContent = mathP.textContent.slice(0, -1);
@@ -175,35 +220,63 @@ document.getElementById('delete').onclick = () => {
 window.addEventListener('keydown', (event) => {
     let name = event.key;
     let code = event.code;
-    console.log(event.key)
     if (event.key >= 0 && event.key <= 9){
-        mathP.textContent += event.key;
-        number += event.key;
-        totalP.textContent = operate(operator, lockedNum, (+number));
+        if(total){
+            number = lockedNum;
+            lockedNum = '';
+            mathP.textContent += event.key;
+            number += event.key;
+            totalP.textContent = '';
+            total = false;
+        } else {
+            mathP.textContent += event.key;
+            number += event.key;
+            totalP.textContent = operate(operator, lockedNum, (+number));
+        }
     } else if (event.key === 'Enter' || event.key === '='){
-        total = true;
-        mathP.textContent = operate(operator, lockedNum, (+number));
-        totalP.textContent = '';
+        if (lockedNum && number && operator){
+            total = true;
+            mathP.textContent = operate(operator, lockedNum, (+number));
+            totalP.textContent = '';
+        } else { 
+            return;
+        }
     } else if (event.key === "+"){
-        if(mathP.textContent.slice(-1) === '+' || mathP.textContent.slice(-1) === '-' || mathP.textContent.slice(-1) === '÷' || mathP.textContent.slice(-1) === '×'){
+        if (total && lockedNum && operator){
+            runOps(event.key, 'add');
+        } else if (!lockedNum && !number){
+            return;
+        } else if(mathP.textContent.slice(-1) === '+' || mathP.textContent.slice(-1) === '-' || mathP.textContent.slice(-1) === '÷' || mathP.textContent.slice(-1) === '×'){
             return;
         } else {
             runOps(event.key, 'add');
         }
     } else if (event.key === '-'){
-        if(mathP.textContent.slice(-1) === '+' || mathP.textContent.slice(-1) === '-' || mathP.textContent.slice(-1) === '÷' || mathP.textContent.slice(-1) === '×'){
+        if (total && lockedNum && operator){
+            runOps(event.key, 'subtract');
+        } else if (!lockedNum && !number){
+            return;
+        } else if(mathP.textContent.slice(-1) === '+' || mathP.textContent.slice(-1) === '-' || mathP.textContent.slice(-1) === '÷' || mathP.textContent.slice(-1) === '×'){
             return;
         } else {
             runOps(event.key, 'subtract');
         }
     } else if (event.key === '*'){
-        if(mathP.textContent.slice(-1) === '+' || mathP.textContent.slice(-1) === '-' || mathP.textContent.slice(-1) === '÷' || mathP.textContent.slice(-1) === '×'){
+        if (total && lockedNum && operator){
+            runOps('×', 'multiply');
+        } else if (!lockedNum && !number){
+            return;
+        } else if(mathP.textContent.slice(-1) === '+' || mathP.textContent.slice(-1) === '-' || mathP.textContent.slice(-1) === '÷' || mathP.textContent.slice(-1) === '×'){
             return;
         } else {
             runOps('×', 'multiply');
         }
     } else if (event.key ==='/'){
-        if(mathP.textContent.slice(-1) === '+' || mathP.textContent.slice(-1) === '-' || mathP.textContent.slice(-1) === '÷' || mathP.textContent.slice(-1) === '×'){
+        if (total && lockedNum && operator){
+            runOps('÷', 'divide');
+        } else if (!lockedNum && !number){
+            return;
+        } else if(mathP.textContent.slice(-1) === '+' || mathP.textContent.slice(-1) === '-' || mathP.textContent.slice(-1) === '÷' || mathP.textContent.slice(-1) === '×'){
             return;
         } else {
             runOps('÷', 'divide');
@@ -218,6 +291,13 @@ window.addEventListener('keydown', (event) => {
             number += '.';
         }
     } else if (event.key === 'Backspace'){
+        if (total){
+            number = lockedNum.toString();
+            lockedNum = '';
+            totalP.textContent = '';
+            operator = '';
+            total = false;
+        }
         if (number.length > 0){
             number = number.slice(0, -1);
             mathP.textContent = mathP.textContent.slice(0, -1);
